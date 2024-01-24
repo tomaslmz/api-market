214b72316273e8bea886356d5f1601cea053398c
tomaslmz <tomas153lm@gmail.com>
2024-01-24 10:34:32 -0300

    adding supplier photo controller

--- src/controller/SupplierPhotoController.ts
+++ src/controller/SupplierPhotoController.ts
@@ -1,13 +1,42 @@
 import { Request, Response } from 'express';
+import SupplierPhotoRepo from '../repository/SupplierPhotoRepo';
+import SupplierPhoto from '../models/SupplierPhoto';
 
 class SupplierPhotoController {
   async create(req: Request, res: Response) {
     try {
-      if(req.file) {
-        res.status(200).json({
-          response: req.file
-        });
+      if(!req.file) {
+        throw new Error('Photo not found!');
       }
+
+      const newSupplierPhoto = new SupplierPhoto();
+
+      newSupplierPhoto.originalName = req.file.originalname;
+      newSupplierPhoto.filename = req.file.filename;
+      newSupplierPhoto.supplier_id = req.user.id;
+
+      await new SupplierPhotoRepo().save(newSupplierPhoto);
+
+      res.status(200).json({
+        status: 'Uploaded!',
+        message: 'This photo has been upload successfully!'
+      });
+    } catch(err: any) {
+      res.status(500).json({
+        status: 'Internal server error!',
+        message: err.message
+      });
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      await new SupplierPhotoRepo().delete(req.user.id);
+
+      res.status(200).json({
+        status: 'Deleted!',
+        message: 'This photo has been deleted successfully!'
+      });
     } catch(err: any) {
       res.status(500).json({
         status: 'Internal server error!',
