9fea703fe2dae3dbaa3fa50a58462624f1b3183f
tomaslmz <tomas153lm@gmail.com>
2024-01-25 15:49:44 -0300

    updating supplier router

--- src/router/SupplierRouter.ts
+++ src/router/SupplierRouter.ts
@@ -1,16 +1,16 @@
 import BaseRoutes from './base/BaseRouter';
 import validate from '../middlewares/validateSchema';
-import isLogged from '../middlewares/loginRequired';
+import { isAdminLogged, isSupplierLogged } from '../middlewares/loginRequired';
 import { createSupplierSchema, updateSupplierSchema } from '../schema/SupplierSchema';
 import SupplierController from '../controller/SupplierController';
 
 class SupplierRoutes extends BaseRoutes {
   public routes(): void {
-    this.router.post('/create', validate(createSupplierSchema), SupplierController.create);
-    this.router.patch('/update', isLogged, validate(updateSupplierSchema), SupplierController.update);
-    this.router.delete('/delete', isLogged, SupplierController.delete);
-    this.router.get('/list', SupplierController.listAll);
-    this.router.get('/search', SupplierController.listById);
+    this.router.post('/create', isAdminLogged, validate(createSupplierSchema), SupplierController.create);
+    this.router.patch('/update', isSupplierLogged, validate(updateSupplierSchema), SupplierController.update);
+    this.router.delete('/delete', isSupplierLogged, SupplierController.delete);
+    this.router.get('/list', isSupplierLogged, SupplierController.listAll);
+    this.router.get('/search', isSupplierLogged, SupplierController.listById);
   }
 }
 
