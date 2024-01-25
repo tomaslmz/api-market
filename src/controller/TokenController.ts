f606a6e8405ea0f8dc9e81cb87c0d263002de2b7
tomaslmz <tomas153lm@gmail.com>
2024-01-25 15:33:12 -0300

    adding token creator to supplier

--- src/controller/TokenController.ts
+++ src/controller/TokenController.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
-import Administrator from '../models/Administrator';
 import jwt from 'jsonwebtoken';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
+import Administrator from '../models/Administrator';
+import Supplier from '../models/Supplier';
 
 declare module 'express-serve-static-core' {
     interface Request {
@@ -32,16 +34,16 @@ class TokenController {
       if(!newAdministrator) {
         throw new Error('Admin not found!');
       }
-
-      const { id } = newAdministrator;
-
+      
       const correctPassword = await newAdministrator.comparePassword(password);
-
+      
       if(!correctPassword) {
         throw new Error('The password is incorrect!');
       }
+      
+      const { id } = newAdministrator;
 
-      const token = jwt.sign({ id, email }, process.env.TOKEN as string, {
+      const token = jwt.sign({ id, email }, process.env.TOKEN_ADMIN as string, {
         expiresIn: process.env.TOKEN_EXPIRATION as string
       });
 
@@ -63,6 +65,57 @@ class TokenController {
       return res.status(500).json({
         status: 'Internal server error!',
         message: err.message
+      });
+    }
+  }
+
+  async createSupplier(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      if(!email || !password) {
+        throw new Error('Insert a valid login!');
+      }
+
+      const newSupplier = await Supplier.findOne({
+        where: {
+          email
+        }
+      });
+
+      if(!newSupplier) {
+        throw new Error('Supplier not found!');
+      }
+
+      const isPasswordCorrect = newSupplier.comparePassword(password);
+
+      if(!isPasswordCorrect) {
+        throw new Error('The password is incorrect!');
+      }
+
+      const { id } = newSupplier;
+
+      const token = jwt.sign({ id, email }, process.env.ADMIN_TOKEN as string, {
+        expiresIn: process.env.TOKEN_EXPIRATION as string
+      });
+
+      req.user = { id, email };
+
+      return res.status(200).json({
+        status: 'Ok!',
+        message: 'Token has been created successfully!',
+        data: {
+          token,
+          user: {
+            id: newSupplier.id,
+            email: newSupplier.email
+          }
+        }
+      });
+    } catch(err: any) {
+      return res.status(500).json({
+        status: 'Internal server error!',
+        message: err.any
       });
     }
   }
