8ac40a17dd12370197526e6a0a8f85a4d2e53394
tomaslmz <tomas153lm@gmail.com>
2024-01-25 16:07:34 -0300

    updating tag router to use supplier middleware

--- src/router/TagRouter.ts
+++ src/router/TagRouter.ts
@@ -1,17 +1,16 @@
 import BaseRoutes from './base/BaseRouter';
-import isLogged from '../middlewares/loginRequired';
+import { isSupplierLogged } from '../middlewares/loginRequired';
 import validate from '../middlewares/validateSchema';
 import { createTagSchema, updateTagSchema } from '../schema/TagSchema';
 import TagController from '../controller/TagController';
-import isAdmin from '../middlewares/adminRequired';
 
 class TagRoutes extends BaseRoutes {
   public routes(): void {
-    this.router.post('/create', isLogged, isAdmin, validate(createTagSchema), TagController.create);
-    this.router.patch('/update/:id', isLogged, isAdmin, validate(updateTagSchema), TagController.update);
-    this.router.delete('/delete/:id', isLogged, isAdmin, TagController.delete);
-    this.router.get('/list', isLogged, TagController.listAll);
-    this.router.get('/search', isLogged, TagController.listByName);
+    this.router.post('/create', isSupplierLogged, validate(createTagSchema), TagController.create);
+    this.router.patch('/update/:id', isSupplierLogged, validate(updateTagSchema), TagController.update);
+    this.router.delete('/delete/:id', isSupplierLogged, TagController.delete);
+    this.router.get('/list', TagController.listAll);
+    this.router.get('/search', TagController.listByName);
   }
 }
 
