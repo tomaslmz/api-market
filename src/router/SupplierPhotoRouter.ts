d8f10889fb81563245ca0e94a1c98a8ff22351aa
tomaslmz <tomas153lm@gmail.com>
2024-01-25 15:51:03 -0300

    adding supplier middleware to supplier photo router

--- src/router/SupplierPhotoRouter.ts
+++ src/router/SupplierPhotoRouter.ts
@@ -1,12 +1,13 @@
 import BaseRoutes from './base/BaseRouter';
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import { isSupplierLogged } from '../middlewares/loginRequired';
 import SupplierPhotoController from '../controller/SupplierPhotoController';
 
 class SupplierPhotoRoutes extends BaseRoutes {
   routes(): void {
-    this.router.post('/upload', multer(multerConfig).single('photo'), SupplierPhotoController.create);
-    this.router.delete('/delete', SupplierPhotoController.delete);
+    this.router.post('/upload', isSupplierLogged, multer(multerConfig).single('photo'), SupplierPhotoController.create);
+    this.router.delete('/delete', isSupplierLogged, SupplierPhotoController.delete);
   }
 }
 
