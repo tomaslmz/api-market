e801ae6594d01f9ef41331ed77464ca87d8e5684
tomaslmz <tomas153lm@gmail.com>
2024-01-29 10:49:51 -0300

    feat: adding middleware to user routes

--- src/router/UserRouter.ts
+++ src/router/UserRouter.ts
@@ -2,13 +2,14 @@ import BaseRoutes from './base/BaseRouter';
 import UserController from '../controller/UserController';
 import { createUserSchema, updateUserSchema } from '../schema/UserSchema';
 import validate from '../middlewares/validateSchema';
+import { isUserLogged } from '../middlewares/loginRequired';
 
 class UserRoutes extends BaseRoutes {
   routes(): void {
     this.router.post('/create', validate(createUserSchema), UserController.create);
-    this.router.patch('/update', validate(updateUserSchema), UserController.update);
-    this.router.delete('/delete', UserController.delete);
-    this.router.get('/list', UserController.listById);
+    this.router.patch('/update', isUserLogged, validate(updateUserSchema), UserController.update);
+    this.router.delete('/delete', isUserLogged, UserController.delete);
+    this.router.get('/list', isUserLogged, UserController.listById);
   }
 }
 
