b07d2b76be50f424a5edd4f11cd0ecc82c30b7c2
tomaslmz <tomas153lm@gmail.com>
2024-01-30 16:02:23 -0300

    feat: adding a seeder to create first admin

---
+++ src/database/seeder/createFirstAdminTest.ts
@@ -0,0 +1,37 @@
+import { Sequelize } from 'sequelize-typescript';
+import env from '../../tests/env';
+
+import Administrator from '../../models/Administrator';
+import Tag from '../../models/Tag';
+import Supplier from '../../models/Supplier';
+import SupplierPhoto from '../../models/SupplierPhoto';
+import User from '../../models/User';
+
+const POSTGRES_DB = env.TEST_POSTGRES_DB;
+const POSTGRES_HOST = env.POSTGRES_HOST;
+const POSTGRES_PORT = env.POSTGRES_PORT;
+const POSTGRES_USER = env.POSTGRES_USER;
+const POSTGRES_PASSWORD = env.POSTGRES_PASSWORD;
+
+const sequelize = new Sequelize({
+  database: POSTGRES_DB,
+  username: POSTGRES_USER,
+  password: POSTGRES_PASSWORD,
+  port: parseInt(POSTGRES_PORT),
+  host: POSTGRES_HOST,
+  dialect: 'postgres',
+  models: [Administrator, Tag, Supplier, SupplierPhoto, User],
+  logging: false
+});
+
+async function create() {
+  await sequelize.sync();
+
+  await Administrator.create({
+    name: 'admin',
+    email: 'admin@admin.com',
+    password: 'admin'
+  });
+}
+
+create();
